02a2049e32767690d1041eb5baf40df280a7246c
linothomas14 <linolowlevel@gmail.com>
2023-08-14 18:53:50 +0700

    add cors library

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const connectDB = require("./config/mongodb.js");
 const express = require('express')
+const cors = require('cors');
 const videosRoutes = require('./routes/video.js')
 const productsRoutes = require('./routes/product.js')
 const commentsRoutes = require('./routes/comment.js')
 
 const app = express();
 
-
+app.use(cors());
 // Parse incoming requests data
 app.use(express.json());
+
 
 app.use("/", videosRoutes);
 app.use("/", productsRoutes);
@@ -17,6 +19,6 @@ app.use("/", commentsRoutes);
 // Connect to MongoDB
 connectDB();
 
-app.listen("3000", () => {
-    console.log('Server is running on port 3000');
+app.listen("5000", () => {
+    console.log('Server is running on port 5000');
 });
